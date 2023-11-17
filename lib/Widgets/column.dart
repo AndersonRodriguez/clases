32b3084dd0ef2ea702e88ea199ded3251afd0055
AndersonRodriguez <anderson.rodri26@gmail.com>
2023-11-16 20:37:20 -0500

    Column y Row

---
+++ lib/Widgets/column.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Screen());
+}
+
+class Screen extends StatelessWidget {
+  const Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Column',
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              verticalDirection: VerticalDirection.down,
+              children: [
+                const Text('Primero'),
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  color: Colors.green,
+                ),
+                const Text(
+                  'Segundo',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                Container(
+                  width: 200.0,
+                  height: 150.0,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
